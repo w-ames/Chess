5f5f372f3339ac605a7296bc80c12877c31d7bb0
Gregory Cal <gregory.cal.stu@kingsu.ca>
2021-10-29 15:23:41 -0600

    Beginning Cardlayout setup

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ApplicationFrame.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ApplicationFrame.java
@@ -8,10 +8,20 @@ public class ApplicationFrame extends JFrame {
     private static final String WINDOW_TITLE = "Chess Game";
     private static final Dimension WINDOW_SIZE = new Dimension(1500, 1000);
 
+    private JPanel contentPanel;
+    private CardLayout layout;
+
     public ApplicationFrame() {
         super(WINDOW_TITLE);
+        contentPanel = new JPanel();
+        layout = new CardLayout();
+
+        contentPanel.setLayout(layout);
+
         setPreferredSize(WINDOW_SIZE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
         pack();
         setVisible(true);
     }
