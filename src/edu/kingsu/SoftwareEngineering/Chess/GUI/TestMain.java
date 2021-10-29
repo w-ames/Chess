b257d23cb6e7234f3e959dce1a26632b2bd60f06
Chelsie Bajic <chelsie.bajic.stu@kingsu.ca>
2021-10-29 15:20:44 -0600

    Update

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/TestMain.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/TestMain.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 
+import com.apple.eawt.Application;
+
 public class TestMain {
 
     public static void main(String[] args) {
-        TestFrame testFrame = new TestFrame();
+        ApplicationFrame testFrame = new ApplicationFrame();
         ChessPanel chessPanel = new ChessPanel();
 
         testFrame.add(chessPanel);
