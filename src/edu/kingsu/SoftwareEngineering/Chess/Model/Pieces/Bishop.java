49d581ed39c7930ee304154cb7f3aaaca9016ed9
Nik <n.haugrud@hotmail.com>
2021-10-29 10:47:05 -0600

    Test partial changes

--- src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/Bishop.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/Bishop.java
@@ -1,6 +1,11 @@
 package edu.kingsu.SoftwareEngineering.Chess.Model.Pieces;
 
-public class Bishop {
+public class Bishop extends Piece {
     public Bishop(boolean isWhite) {
+        super(isWhite);
+    }
+
+    public PieceType getPieceType() {
+        return PieceType.BISHOP;
     }
 }
