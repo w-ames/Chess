5692b925fcb93f55c48f86073f10240ebcf10dd6
Nik <n.haugrud@hotmail.com>
2021-10-31 20:06:43 -0600

    Early piece setup

--- src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/Bishop.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/Bishop.java
@@ -1,10 +1,21 @@
 package edu.kingsu.SoftwareEngineering.Chess.Model.Pieces;
 
+/**
+ * This class represents a bishop in a game of chess.
+ */
 public class Bishop extends Piece {
+
+    /**
+     * Creates a new {@link Bishop} instance.
+     * @param isWhite a boolean which signifies the piece is white if <code>true</code>, black if <code>false</code>
+     */
     public Bishop(boolean isWhite) {
         super(isWhite);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public PieceType getPieceType() {
         return PieceType.BISHOP;
--- src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/King.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/Model/Pieces/King.java
@@ -1,10 +1,21 @@
 package edu.kingsu.SoftwareEngineering.Chess.Model.Pieces;
 
+/**
+ * This class represents a king in a game of chess.
+ */
 public class King extends Piece {
+
+    /**
+     * Creates a new {@link King} instance.
+     * @param isWhite a boolean which signifies the piece is white if <code>true</code>, black if <code>false</code>
+     */
     public King(boolean isWhite) {
         super(isWhite);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public PieceType getPieceType() {
         return PieceType.KING;
