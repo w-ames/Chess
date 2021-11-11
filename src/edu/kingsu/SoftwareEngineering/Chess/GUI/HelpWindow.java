8d81cd0a72ba9c349bad0077d1180a212899a4c0
Chelsie Bajic <chelsie.bajic.stu@kingsu.ca>
2021-11-10 22:34:24 -0700

    Started help/about dialog

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/HelpWindow.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/HelpWindow.java
@@ -1,12 +1,42 @@
 package edu.kingsu.SoftwareEngineering.Chess.GUI;
 
+import java.awt.Color;
 import javax.swing.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import edu.kingsu.SoftwareEngineering.Chess.Model.*;
 
 public class HelpWindow extends JFrame {
+
+    MainLayer background = new MainLayer(); 
+
     public HelpWindow() {
     }
 
     public HelpWindow(String orgin){
+
+        this.setLayout(new GridLayout(0, 1));
+        this.add(background);
 
     }
 }
