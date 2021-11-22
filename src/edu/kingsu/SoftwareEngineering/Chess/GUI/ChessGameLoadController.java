5a0e40f66c2eb3a8cadfd30dd629499208939225
Will Ames <william.ames.stu@kingsu.ca>
2021-11-22 15:16:51 -0700

    Started writing save controller

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameLoadController.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameLoadController.java
@@ -6,6 +6,11 @@ import java.awt.event.*;
 public class ChessGameLoadController implements ActionListener{
     
     public void actionPerformed(ActionEvent e){
+        JFileChooser chooser= new JFileChooser();
+        FileNameExtensionFilter filter= new FileNameExtensionFilter("PGN files", "pgn");
+        chooser.setTileFilter(filter);
+        fileChooser.setDialogTitle("Load Chess Game");
+
         
     }
 }
--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameSaveController.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameSaveController.java
@@ -2,10 +2,26 @@ package edu.kingsu.SoftwareEngineering.Chess.GUI;
 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ChessGameSaveController implements ActionListener{
     
     public void actionPerformed(ActionEvent e){
-        
+        JFileChooser chooser= new JFileChooser();
+        FileNameExtensionFilter filter= new FileNameExtensionFilter("PGN files", "pgn");
+        chooser.setTileFilter(filter);
+        fileChooser.setDialogTitle("Save Chess Game");
+
+        //get the application frame via the parents of the object which this event
+        //occurred on
+        Container container= e.getSource();
+        while(container.getParent() != null){
+            container= container.getParent();
+        }
+        int userSelection= chooser.showSaveDialog(container);  //how do I specify the parent here?
+        if(userSelection == JFileChooser.APPROVE_OPTION){
+            File fileToSave= chooser.getSelectedFile();
+        }
     }
 }
