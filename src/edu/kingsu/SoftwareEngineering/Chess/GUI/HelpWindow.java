c045c1340315fd52ca2719ad51f03ef1884c8352
Chelsie Bajic <chelsie.bajic.stu@kingsu.ca>
2021-11-21 21:00:30 -0700

    Update license

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/HelpWindow.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/HelpWindow.java
@@ -241,19 +241,24 @@ public class HelpWindow extends JFrame {
         silverPanel.add(textArea, gb);
 
         textArea.setLayout(new GridBagLayout());
-        JLabel licenseText = new JLabel("<html>" + "<span> Copyright 2021 Java Chess Team</span>" + "<br>" + "<br>"
-                + "<p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>"
-                + "<br>"
-                + "<p>1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</p>"
-                + "<br>"
-                + "<p>2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</p>"
-                + "<br"
-                + "<p>3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</p>"
-                + "<br"
-                + "<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>"
-                + "</html>");
+        JEditorPane licensePane = new JEditorPane();
+        File file = new File("src/assets/license.html");
+        try {
+            licensePane.setPage(file.toURI().toURL());
+        } catch (MalformedURLException e) {
+            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        licensePane.setEditable(false);
+        licensePane.setOpaque(false);
 
-        textArea.add(licenseText, gb);
+        JScrollPane editorScrollPane = new JScrollPane(licensePane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        editorScrollPane.setOpaque(false);
+        textArea.add(editorScrollPane, gb);
+
+        textArea.add(editorScrollPane, gb);
 
     }
 
