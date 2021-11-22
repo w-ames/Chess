33ed80d93ceefe3453112296a00da712cc67e03b
Will Ames <william.ames.stu@kingsu.ca>
2021-11-22 14:15:18 -0700

    Created skeleton ChessGameSaveController and ChessGameLoadController classes

---
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameLoadController.java
@@ -0,0 +1,11 @@
+package edu.kingsu.SoftwareEngineering.Chess.GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChessGameLoadController implements ActionListener{
+    
+    public void actionPerformed(ActionEvent e){
+        
+    }
+}
