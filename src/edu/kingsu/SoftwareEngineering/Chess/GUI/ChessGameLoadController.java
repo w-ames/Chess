cf6d1986baedbcb310566e06927f9e718a8eb7eb
Will Ames <william.ames.stu@kingsu.ca>
2021-11-23 00:39:42 -0700

    Wrote rough draft of save and load controllers

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameLoadController.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameLoadController.java
@@ -3,14 +3,36 @@ package edu.kingsu.SoftwareEngineering.Chess.GUI;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import edu.kingsu.SoftwareEngineering.Chess.PGN.PGNFile;
+
 public class ChessGameLoadController implements ActionListener{
-    
+    private ChessGame chessGame;
+
+    public ChessGameLoadController(ChessGame chessGame){
+        this.chessGame= chessGame;
+    }
+
     public void actionPerformed(ActionEvent e){
         JFileChooser chooser= new JFileChooser();
         FileNameExtensionFilter filter= new FileNameExtensionFilter("PGN files", "pgn");
         chooser.setTileFilter(filter);
         fileChooser.setDialogTitle("Load Chess Game");
 
-        
+        //get the application frame via the parents of the object which this event
+        //occurred on
+        Container container= e.getSource();
+        while(container.getParent() != null){
+            container= container.getParent();
+        }
+
+        int userSelection= chooser.showOpenDialog(container);
+        if(userSelection == JFileChooser.APPROVE_OPTION){
+            File selectedFile= chooser.getSelectedFile();
+            PGNFile pgnFile= new PGNFile(selectedFile);
+            //how to set pgnFile as the member in GameSetUp?
+        }
     }
 }
--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameSaveController.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameSaveController.java
@@ -2,11 +2,20 @@ package edu.kingsu.SoftwareEngineering.Chess.GUI;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileWriter;
+
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import edu.kingsu.SoftwareEngineering.Chess.PGN.PGNFile;
+
 public class ChessGameSaveController implements ActionListener{
-    
+    private ChessGame chessGame;
+
+    public ChessGameSaveController(ChessGame chessGame){
+        this.chessGame= chessGame;
+    }
+
     public void actionPerformed(ActionEvent e){
         JFileChooser chooser= new JFileChooser();
         FileNameExtensionFilter filter= new FileNameExtensionFilter("PGN files", "pgn");
@@ -19,9 +28,20 @@ public class ChessGameSaveController implements ActionListener{
         while(container.getParent() != null){
             container= container.getParent();
         }
-        int userSelection= chooser.showSaveDialog(container);  //how do I specify the parent here?
+
+        int userSelection= chooser.showSaveDialog(container);
         if(userSelection == JFileChooser.APPROVE_OPTION){
             File fileToSave= chooser.getSelectedFile();
+            try{
+                PGNFile pgnRepresentation= new PGNFile(chessGame.getTagPairMap(), chessGame.getAlgebraicHistory(), chessGame.getResult());  //ChessGame needs to store the result
+                String pgn= pgnRepresentation.getFileText();
+
+                FileWriter writer= new FileWriter(fileToSave);
+                writer.write(pgn);
+                writer.close();
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
         }
     }
 }
