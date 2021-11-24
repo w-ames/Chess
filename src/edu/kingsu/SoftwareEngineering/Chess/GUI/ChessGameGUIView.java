1042e0f0c1b306d9d7fa56d19ecd3de8c3f8c166
Chelsie Bajic <chelsie.bajic.stu@kingsu.ca>
2021-11-24 10:33:52 -0700

    Dynamic board size

--- src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameGUIView.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/GUI/ChessGameGUIView.java
@@ -207,7 +207,7 @@ public class ChessGameGUIView extends ChessGameView {
         // boardHolder.setMaximumSize(new Dimension(550, 550));
         // }
 
-        int size = (int) (currentFrameWidth / 3);
+        int size = (int) (currentFrameWidth / 2.5);
 
         boardHolder.setMaximumSize(new Dimension(size, size));
         boardHolder.setPreferredSize(new Dimension(size, size));
